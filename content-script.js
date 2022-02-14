23fa352c782aea38bd365a1c8c8b2e3ca6117ef7
Ethan  Hodges <ethan@Ethans-HackBook-Pro.local>
2022-02-14 13:37:19 -0500

    fixed the issues

--- content-script.js
+++ content-script.js
@@ -3,22 +3,21 @@ const bodytag = document.getElementsByTagName("body")[0]
 const daGoose = document.createElement("img")
 daGoose.id = "daGoose"
 let count = 0
-let distance = -30
-daGoose.src = chrome.runtime.getURL("0.png");
+let distance = -23
+daGoose.src = chrome.runtime.getURL("images/0-removebg-preview.png");
 daGoose.style.position = "absolute"
 daGoose.style.bottom ="0"
 daGoose.style.right = "-100px"
 bodytag.appendChild(daGoose)
 const myInterval = setInterval(()=>{
-    const filename = String(count) + ".png"
-    console.log(filename)
+    const filename = "images/" + String(count) + "-removebg-preview.png"
     const pixeldistance = distance * 3
     daGoose.src = chrome.runtime.getURL(filename)
     daGoose.style.right = String(pixeldistance) + "px"
     count++
     distance++
     if (pixeldistance > bodytag.clientWidth){
-        distance = -30
+        distance = -23
     }
     count = count % 10
 },125)
